e6520c6bb08c42c54112b8dc6d14cc0c940a02bd
ConorJAT <conorjat75@gmail.com>
2024-02-13 16:24:35 -0500

    Implemented code into server.js and allowed eslint correct code.

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,67 +1,67 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json'});
-    response.write(object);
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(object);
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-    response.writeHead(status, { 'Content-Type': 'application/json'});
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
 };
 
 const getUsers = (request, response) => {
-    const responseJSON = { users };
+  const responseJSON = { users };
 
-    respondJSON(request, response, 200, JSON.stringify(responseJSON));
+  respondJSON(request, response, 200, JSON.stringify(responseJSON));
 };
 
 const getUsersMeta = (request, response) => {
-    respondJSONMeta(request, response, 200);
+  respondJSONMeta(request, response, 200);
 };
 
 const notReal = (request, response) => {
-    const responseJSON = { message: 'The page you are looking for was not found!', id: 'notFound' };
+  const responseJSON = { message: 'The page you are looking for was not found!', id: 'notFound' };
 
-    respondJSON(request, response, 404, JSON.stringify(responseJSON));
+  respondJSON(request, response, 404, JSON.stringify(responseJSON));
 };
 
 const notRealMeta = (request, response) => {
-    respondJSONMeta(request, response, 404);
+  respondJSONMeta(request, response, 404);
 };
 
 const addUser = (request, response, body) => {
-    const responseJSON = {
-        message: 'Name and age are both required!',
-    };
-    
-    if (!body.name || !body.age) {
-        responseJSON.id = 'addUserMissingParams';
-        return respondJSON(request, response, 400, JSON.stringify(responseJSON));
-    }
-    
-    let responseCode = 204;
-    
-    if (!users[body.name]) {
-        responseCode = 201;
-        users[body.name] = {};
-    }
-    
-    users[body.name].name = body.name;
-    users[body.name].age = body.age;
-    
-    if (responseCode === 204) {
-        return respondJSONMeta(request, response, 204);
-    }
-    
-    return respondJSON(request, response, 201, JSON.stringify(users[body.name]));
+  const responseJSON = {
+    message: 'Name and age are both required!',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'addUserMissingParams';
+    return respondJSON(request, response, 400, JSON.stringify(responseJSON));
+  }
+
+  let responseCode = 204;
+
+  if (!users[body.name]) {
+    responseCode = 201;
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 204) {
+    return respondJSONMeta(request, response, 204);
+  }
+
+  return respondJSON(request, response, 201, JSON.stringify(users[body.name]));
 };
 
 module.exports = {
-    getUsers,
-    getUsersMeta,
-    notReal,
-    notRealMeta,
-    addUser
+  getUsers,
+  getUsersMeta,
+  notReal,
+  notRealMeta,
+  addUser,
 };
--- src/server.js
+++ src/server.js
@@ -6,24 +6,64 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-urlStruct = {
-    'GET': {
-        '/': htmlHandler.getIndex,
-        '/style.css': htmlHandler.getCSS,
-        '/getUsers': jsonHandler.getUsers,
-        notReal: jsonHandler.notReal
-    },
-    'HEAD': {
-        '/getUsers': jsonHandler.getUsersMeta,
-        notReal: jsonHandler.notRealMeta
-    },
-    'POST': {
-        '/addUsers': jsonHandler.addUser
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    // application/x-www-form-urlencoded (key1=value1&key2=value2)
+    const bodyString = Buffer.concat(body).toString();
+    let bodyParams;
+
+    if (request.headers['content-type'] === 'application/json') {
+      bodyParams = JSON.parse(bodyString);
+    } else {
+      bodyParams = query.parse(bodyString);
     }
-}
+
+    handler(request, response, bodyParams);
+  });
+};
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    notReal: jsonHandler.notReal,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notReal: jsonHandler.notRealMeta,
+  },
+  POST: {
+    '/addUsers': parseBody,
+  },
+};
 
 const onRequest = (request, response) => {
+  const parsedURL = url.parse(request.url);
 
+  const methodHandler = urlStruct[request.method];
+  if (!methodHandler) {
+    urlStruct.HEAD.notReal(request, response);
+  }
+
+  const handlerFunc = methodHandler[parsedURL.pathname];
+  if (handlerFunc) {
+    handlerFunc(request, response, jsonHandler.addUser);
+  } else {
+    urlStruct.GET.notReal(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => { console.log(`Listening on 127.0.0.1:${port}`); });
